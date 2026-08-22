2014c18c580b4af8736c3b88c396bee01114251e
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-08-22 16:22:29 +0530

    Complete Assignment 2

--- class 1/lecture2.py
+++ class 1/lecture2.py
@@ -2,30 +2,33 @@
 name="daksh"
 age="19"
 city="neemuch"
+print(name,age,city)
 
 # question2
 name="daksh"
 roll_number="112510"
 student_branch="cse with AI/ML"
-
+print(name,roll_number,student_branch)
 
 # question 3
 marks="88"
 marks="100"
+print(marks)
 
 # question 4
 name,age,city="rahul",18,"patna"
-
+print(name,age,city)
 
 # question 5
 x=y=z= "0"
+print(x,y,z)
 
 
 # question 6
 student1="Rahul"
 student_name="Rahul"
 cource="B'tech"
-
+print(student1,student_name,cource)
 
 # question 7
 #student-information
@@ -33,3 +36,5 @@ name="daksh"
 age="18"
 age="19"
 student_cource="B.tech"
+print(name,age,student_cource)
+
