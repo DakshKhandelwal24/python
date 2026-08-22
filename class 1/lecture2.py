b4a9c041c99bacc60304ed2882c4351a96b41735
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-08-22 15:57:22 +0530

    hw question added

---
+++ class 1/lecture2.py
@@ -0,0 +1,35 @@
+#student-information
+name="daksh"
+age="19"
+city="neemuch"
+
+# question2
+name="daksh"
+roll_number="112510"
+student_branch="cse with AI/ML"
+
+
+# question 3
+marks="88"
+marks="100"
+
+# question 4
+name,age,city="rahul",18,"patna"
+
+
+# question 5
+x=y=z= "0"
+
+
+# question 6
+student1="Rahul"
+student_name="Rahul"
+cource="B'tech"
+
+
+# question 7
+#student-information
+name="daksh"
+age="18"
+age="19"
+student_cource="B.tech"
