306ba10ade8020b17530452e9c4156f991b6d699
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-08-24 13:43:06 +0530

    file added

---
+++ 03 data type and data casting/classwork3.py
@@ -0,0 +1,13 @@
+# student information
+
+Name="anil" 
+Age="22"
+height="5.6"
+age=22
+age=str(age)
+print(type(age))
+# this is know as dry run 
+Age=11
+print(type(Age))
+age=int(age)
+print(string_to_int)
