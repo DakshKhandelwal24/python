0d6783743b8e2d0720860eb8b413084bf30c5ee5
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-08-24 18:05:36 +0530

    assignment and classwork added

---
+++ 03 data type and data casting/assignment3.py
@@ -0,0 +1,59 @@
+student_name ="daksh"
+student_age =19
+student_height =5.9
+is_student=False
+result=None
+print(type(student_name))
+print(type(student_age))
+print(type(student_height))
+print(type(is_student))
+print(type(result))
+# question 2
+a=50
+b=50.0
+c="50"
+print(type(a))
+print(type(b))
+print(type(c))
+# Question3
+a=True
+b="True"
+print(type(a))
+print(type(b))
+# Question 3
+a=None
+b="None"
+print(type(a))
+print(type(b))
+# Question 4
+value=55
+print(type(value))
+value="Javascript"
+print(type(value))
+# there datatype change from int to str
+# Question 5
+product_name="bike"
+proguct_quantity=1
+product_price=1000000.00
+is_it_boolean=True
+product_discount=None
+print(type(product_name))
+print(type(proguct_quantity))
+print(type(product_discount))
+print(type(is_it_boolean))
+print(type(product_discount))
+# Question 6
+a=10
+b=10.0
+c="10"
+d=True
+e="True"
+f=None
+g="None"
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
