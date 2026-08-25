c6331573677a7b9e20bed1a23410a17bfb8a8d5b
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-08-25 16:26:36 +0530

    classwork added

---
+++ 04 Arthmetic_operators/class4.py
@@ -0,0 +1,20 @@
+# Arthematic operation
+a=10
+b=5
+print(a+b)
+
+c="daksh"
+d=10
+# print(str(d)+c) iddhar convert kara no. ko string ma 
+
+# divide of integer always give flot Value
+print(True+True)
+g=15
+h=20
+print(g-h)
+aa=46
+bb=90
+print(aa//bb)
+
+
+
