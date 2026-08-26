bd96601cd8ad8810c7bbc4e30a8e7a5187bcd41b
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-08-26 13:00:51 +0530

    classwork added

---
+++ 05 python string operation/05classwork.py
@@ -0,0 +1,20 @@
+print(-10%3)
+print("we are in \n kalol")
+print('"i love ' \
+'mumbai"')
+print("")
+a="python"
+print(a[3])
+print(a[-6])
+print(len(a))
+print(a[0:3])
+print(a[0:])
+print(a[:4])
+print(a[:])
+first_name="daksh"
+last_name="khandelwal"
+full_name= first_name + " " + last_name
+print(full_name) 
+print(a[0:5:2])
+print(a[ : :-1])
+print(a[0:7])
