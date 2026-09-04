8bddd791ede5fe88f79b52859d61808a0e3979c1
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-04 12:33:10 +0530

    changes added

--- 07 input and output/classwork07.py
+++ 07 input and output/classwork07.py
@@ -1,7 +1,11 @@
-#input 
-age=input("entre your age: ")
-print(type(age))
-#this ask question from users
-first , last=input("entre first and last name").split()
-print(first)
-print(last)
+# #input 
+# age=input("entre your age: ")
+# print(type(age))
+# #this ask question from users
+# first , last=input("entre first and last name").split()
+# print(first)
+# print(last)
+date=input("entre the date")
+month= 12
+year=2026
+print(date ,"\n", month ,"\n" ,year ,"\n")
