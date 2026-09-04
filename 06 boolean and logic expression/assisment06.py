c8898d19444bdebf59af388ffa9e93563de64e31
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-04 17:18:34 +0530

    added

---
+++ 06 boolean and logic expression/assisment06.py
@@ -0,0 +1,35 @@
+print(bool(0),
+      bool(1),
+      bool(-5),
+      bool(""),
+      bool("Python"),
+      bool(False),
+      bool(True),
+      bool(None)
+)
+
+#Task 29
+print(bool(0),
+      bool(10),
+      bool(""),
+      bool("Hello"),
+      bool(None),
+
+)
+
+#Task 30
+print(type(0),
+      bool(0),
+      type(1),
+      bool(1),
+      type(""),
+      bool(""),
+      type("Python"),
+      bool("Python"),
+      type(False),
+      bool(False),
+      type(True),
+      bool(True),
+      type(None),
+      bool(None)
+)
