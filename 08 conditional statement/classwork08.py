59ee856d84958a56ebddd53a32c3b9f2078f9d1a
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-05 12:27:21 +0530

    modification added

--- 08 conditional statement/classwork08.py
+++ 08 conditional statement/classwork08.py
@@ -5,4 +5,16 @@
 #     print("passs!!")
 
 # else :
-#     print("fail!!")
+# #     print("fail!!")
+
+is_indian=input("Are you indian ? yes or no:")
+
+if is_indian=="yes":
+    age=int(input("Enter your age:-"))
+    if age>=18:
+        print("allowed to vote")
+    if age<18:
+        print("you are not elligable for vote rn")
+
+if is_indian=="no":
+        print("you are not allowed to vote")
