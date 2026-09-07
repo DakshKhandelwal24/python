1557b2d7225b95e690734b385bbd66e7513461b7
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-07 18:04:49 +0530

    added

--- 08 conditional statement/homework8.py
+++ 08 conditional statement/homework8.py
@@ -1,113 +1,167 @@
-#question 1
-num=int(input("Enter number:-"))
-if num>=10:
-    print("true")
-else :
-    print("false")
+# #question 1
+# num=int(input("Enter number:-"))
+# if num>=10:
+#     print("true")
+# else :
+#     print("false")
 
-#question 2
-age=int(input("Enter your age:-"))
-if age>=18:
-    print("adult")
-else :
-    print("bye bye ")
+# #question 2
+# age=int(input("Enter your age:-"))
+# if age>=18:
+#     print("adult")
+# else :
+#     print("bye bye ")
 
-#question 3
-num=int(input("Enter the number:-"))
-if num>0:
-    print("positive")
+# #question 3
+# num=int(input("Enter the number:-"))
+# if num>0:
+#     print("positive")
 
-#question 4
-marks=int(input("Enter marks:-"))
-if marks>=40:
-    print("pass")
+# #question 4
+# marks=int(input("Enter marks:-"))
+# if marks>=40:
+#     print("pass")
 
-#question 5 
-num=int(input("Enter the number:-"))
-if num==0:
-    print("zero")
+# #question 5 
+# num=int(input("Enter the number:-"))
+# if num==0:
+#     print("zero")
 
-#question 6
-num=int(input("Enter the number:-"))
-if num>=0:
-    print("positive")
-else :
-    print("negative")
+# #question 6
+# num=int(input("Enter the number:-"))
+# if num>=0:
+#     print("positive")
+# else :
+#     print("negative")
 
-#question 7
-age=int(input("Enter your age:-"))
-if age>=18:
-    print("adult")
-else :
-    print("minor")
+# #question 7
+# age=int(input("Enter your age:-"))
+# if age>=18:
+#     print("adult")
+# else :
+#     print("minor")
 
-#question 8
-num= int(input("Enter number:-"))
-if num % 2==0:
-    print("even")
-else :
-    print("odd")
+# #question 8
+# num= int(input("Enter number:-"))
+# if num % 2==0:
+#     print("even")
+# else :
+#     print("odd")
 
-#question 9
-marks=int(input("Enter your marks:-"))
-if marks>=40:
-    print("pass")
-else :
-    print("fail")
+# #question 9
+# marks=int(input("Enter your marks:-"))
+# if marks>=40:
+#     print("pass")
+# else :
+#     print("fail")
 
-#question 10
-num1=int(input("Enter number 1 :-"))
-num2=int(input("Enter number 2 :-"))
-if num1>num2:
-    print(num1)
-else :
-    print(num2)
+# #question 10
+# num1=int(input("Enter number 1 :-"))
+# num2=int(input("Enter number 2 :-"))
+# if num1>num2:
+#     print(num1)
+# else :
+#     print(num2)
 
-#question 11
-marks=int(input("Enter the number:-"))
-if marks>=90:
-    print("A")
-if marks>=75:
-    if marks<=89:
-        print("B")
-if marks>=60:
-    if marks<=74:
-        print("c")
-if marks>=40:
-    if marks<=59:
-        print("D")
-else :
-    print("F")
+# #question 11
+# marks=int(input("Enter the number:-"))
+# if marks>=90:
+#     print("A")
+# if marks>=75:
+#     if marks<=89:
+#         print("B")
+# if marks>=60:
+#     if marks<=74:
+#         print("c")
+# if marks>=40:
+#     if marks<=59:
+#         print("D")
+# else :
+#     print("F")
 
-#question 12
-num=int(input("Enter the number:-"))
-if num>0:
-    print("Positive")
-if num<0:
-    print("Negetive")
-if num==0:
-    print("Zero")
+# #question 12
+# num=int(input("Enter the number:-"))
+# if num>0:
+#     print("Positive")
+# if num<0:
+#     print("Negetive")
+# if num==0:
+#     print("Zero")
 
-#question 13
-num=int(input("Enter the number:-"))
-if num==1:
-    print("monday")
-if num==2:
-    print("Tuesday")
-if num==3:
-    print("wednesday")
-if num==4:
-    print("thursday")
-if num==5:
-    print("friday")
+# #question 13
+# num=int(input("Enter the number:-"))
+# if num==1:
+#     print("monday")
+# if num==2:
+#     print("Tuesday")
+# if num==3:
+#     print("wednesday")
+# if num==4:
+#     print("thursday")
+# if num==5:
+#     print("friday")
 
-#question 14
-num=int(input("Enter the number:-"))
-if num==80:
-    print("Excellent")
-if num==60:
-    print("good")
-if num==40:
-    print("pass")
-if num<40:
-    print("fail")
+# #question 14
+# num=int(input("Enter the number:-"))
+# if num==80:
+#     print("Excellent")
+# if num==60:
+#     print("good")
+# if num==40:
+#     print("pass")
+# if num<40:
+#     print("fail")
+#     # question 30
+# age=int(input("entre your age"))
+# if age>=18:
+#         marks=int(input("entre your marks"))
+#         if marks>=40:
+#             has_id=bool(input("entre you have id or not by True and false"))
+#             if has_id== True:
+#                print("Eligible")
+#             else :
+#                 print("Not eligible")
+# else :
+#     print("not eligible")
+#question 29
+# student=bool(input("entre true or false":))
+# if student==True:
+#     has_id=bool(input("entre that you haveid or not":))
+#     if has_id==True:
+#         has_ticket=bool(input("entre that you have ticket in true or false":))
+#         if has_ticket==True:
+#             print("allowed")
+# else:
+#     print("not allowed")
+
+# question 28
+# number=int(input("entre the number"))
+# if number>=10 and number<=50:
+#     print("its in range")
+# else :
+#     print("its not in range")
+# # question 27
+# number = int(input("Enter a number: "))
+
+# if number < 10 or number > 50:
+#     print("Outside the range")
+# else:
+#     print("Inside the range")
+# question 26
+# is_closed = False
+# if not is_closed==True:
+#     print(open)
+# # question 25
+# number=int(input("entre any number"))
+# if number<0 or number>100 :
+#     print("right_guess")
+# else :
+#     print("wrong_guess")    
+# quesstion 24
+
+# number= int(input("entre any number"))
+# if number>10:
+#     number=int(input("entre second number"))
+# if number>10:
+#         print("both number are greater than 10")
+
