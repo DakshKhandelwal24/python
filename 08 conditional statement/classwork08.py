18854df2b50c6ec94b0c6f92ac7c73d41b711cbb
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-07 10:46:11 +0530

    added hw

--- 08 conditional statement/classwork08.py
+++ 08 conditional statement/classwork08.py
@@ -7,14 +7,26 @@
 # else :
 # #     print("fail!!")
 
-is_indian=input("Are you indian ? yes or no:")
+# is_indian=input("Are you indian ? yes or no:")
 
-if is_indian=="yes":
-    age=int(input("Enter your age:-"))
-    if age>=18:
-        print("allowed to vote")
-    if age<18:
-        print("you are not elligable for vote rn")
+# if is_indian=="yes":
+#     age=int(input("Enter your age:-"))
+#     if age>=18:
+#         print("allowed to vote")
+#     if age<18:
+#         print("you are not elligable for vote rn")
 
-if is_indian=="no":
-        print("you are not allowed to vote")
+# if is_indian=="no":
+#         print("you are not allowed to vote")
+
+# is_indian=input("Are you indian ? yes or no:")
+
+# if is_indian =="yes":
+#     print("welcome to our team")
+#     if  age==input("entre your age"):
+#         if janam_ummar>=18:
+#             print("aa jao team ma")
+#         else :
+#             print("team se bahar")   
+# else :
+#     print("no space in our team")
