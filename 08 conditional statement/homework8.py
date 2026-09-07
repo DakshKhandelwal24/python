06e99c2cb910f2021891b0fb6f8b391441b4dc10
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-07 10:47:01 +0530

    added hw

---
+++ 08 conditional statement/homework8.py
@@ -0,0 +1,113 @@
+#question 1
+num=int(input("Enter number:-"))
+if num>=10:
+    print("true")
+else :
+    print("false")
+
+#question 2
+age=int(input("Enter your age:-"))
+if age>=18:
+    print("adult")
+else :
+    print("bye bye ")
+
+#question 3
+num=int(input("Enter the number:-"))
+if num>0:
+    print("positive")
+
+#question 4
+marks=int(input("Enter marks:-"))
+if marks>=40:
+    print("pass")
+
+#question 5 
+num=int(input("Enter the number:-"))
+if num==0:
+    print("zero")
+
+#question 6
+num=int(input("Enter the number:-"))
+if num>=0:
+    print("positive")
+else :
+    print("negative")
+
+#question 7
+age=int(input("Enter your age:-"))
+if age>=18:
+    print("adult")
+else :
+    print("minor")
+
+#question 8
+num= int(input("Enter number:-"))
+if num % 2==0:
+    print("even")
+else :
+    print("odd")
+
+#question 9
+marks=int(input("Enter your marks:-"))
+if marks>=40:
+    print("pass")
+else :
+    print("fail")
+
+#question 10
+num1=int(input("Enter number 1 :-"))
+num2=int(input("Enter number 2 :-"))
+if num1>num2:
+    print(num1)
+else :
+    print(num2)
+
+#question 11
+marks=int(input("Enter the number:-"))
+if marks>=90:
+    print("A")
+if marks>=75:
+    if marks<=89:
+        print("B")
+if marks>=60:
+    if marks<=74:
+        print("c")
+if marks>=40:
+    if marks<=59:
+        print("D")
+else :
+    print("F")
+
+#question 12
+num=int(input("Enter the number:-"))
+if num>0:
+    print("Positive")
+if num<0:
+    print("Negetive")
+if num==0:
+    print("Zero")
+
+#question 13
+num=int(input("Enter the number:-"))
+if num==1:
+    print("monday")
+if num==2:
+    print("Tuesday")
+if num==3:
+    print("wednesday")
+if num==4:
+    print("thursday")
+if num==5:
+    print("friday")
+
+#question 14
+num=int(input("Enter the number:-"))
+if num==80:
+    print("Excellent")
+if num==60:
+    print("good")
+if num==40:
+    print("pass")
+if num<40:
+    print("fail")
