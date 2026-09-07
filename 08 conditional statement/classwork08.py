687f23466ba96b7c132ea9eb06e7c0d31cc79f4a
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-07 12:55:06 +0530

    added

--- 08 conditional statement/classwork08.py
+++ 08 conditional statement/classwork08.py
@@ -30,3 +30,48 @@
 #             print("team se bahar")   
 # else :
 #     print("no space in our team")
+# number =int(input("Enter the number:-"))
+# if number >=90:
+#     print("A")
+# if number >=75:
+#     if number <=89:
+#         print("B")
+# if number >=60:
+#     if number <=74:
+#         print("c")
+# if number >=40:
+#     if number <=59:
+#         print("D")
+# else :
+#     print("F")
+# marks=int(input( "entre your marks"))
+# if marks>=90:
+#     print("excellent")
+# elif marks>= 60:
+#     print("good")
+# elif marks>= 40:
+#     print("pass")
+# else :
+#     print("fail")    
+# operation=int(input("entre number between 1 to 4"))
+# if operation == "1":
+#     print("addition")
+# elif operation =="2":
+#     print("subtraction")
+# elif operation == "3":
+#     print("multiplication")
+# else :
+#     print("dividion")
+# number1 = int(input("entre first number"))
+# number2 = int(input("entre anouther number"))
+# if operation ==1 and number1 and number2:
+#     print(number1 + number2)
+
+# if operation ==2 and number1 and number2:
+#     print(number1 - number2)
+
+# if operation ==3 and number1 and number2:
+#     print(number1 * number2)
+# elif operation ==4 and number1 and number2:
+#     print(number1 / number2)
+
