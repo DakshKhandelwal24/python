b0b79a4fb21a3a37bcc775c04b3be45053960f23
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-08 17:21:01 +0530

    added

--- 09 problem solving/class.py
+++ 09 problem solving/class.py
@@ -1,4 +1,23 @@
-number=int(input("entre first number"))
-number2=int(input("entre second number"))
-number3=int(input("enyre third number"))
-print(number + number2 +number3 )
+# number=int(input("entre first number"))
+# number2=int(input("entre second number"))
+# number3=int(input("enyre third number"))
+# print(number + number2 +number3 )
+# q 1
+# number=int(input("entre any integer"))
+# if number>0 :
+#     print("positive")
+# if number<0 :
+#     print("negative")
+# if number==0 :
+#     print("zero")        
+
+
+
+# q 2
+number=int(input("entre any integer"))
+if number%2==0 and number>0:
+    print("Positive even")
+
+
+
+
