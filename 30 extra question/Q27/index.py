29de0c9339363802692f0431cdfe9f2490480ca4
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-09 16:11:48 +0530

    added soln

--- 30 extra question/Q27/index.py
+++ 30 extra question/Q27/index.py
@@ -1,3 +1,10 @@
-if __name__ == '__main__':
-    pass
+hour=int(input("entre your input"))
+minute=int(input("entre your input"))
+second=int(input("entre your input"))
 
+if hour>=0 and hour<=23:
+  if minute>=0 and minute<=59:
+    if second>=0 and minute<=59:
+       print("valid time")
+else :
+    print("time is valid")    
--- 30 extra question/Q28/index.py
+++ 30 extra question/Q28/index.py
@@ -1,3 +1,37 @@
-if __name__ == '__main__':
-    pass
+name1 = input("Enter Person 1 name: ")
+age1 = int(input("Enter Person 1 age: "))
 
+name2 = input("Enter Person 2 name: ")
+age2 = int(input("Enter Person 2 age: "))
+
+name3 = input("Enter Person 3 name: ")
+age3 = int(input("Enter Person 3 age: "))
+
+if age1 == age2 == age3:
+    print("All three people are the same age")
+
+elif age1 == age2:
+    if age1 < age3:
+        print(name1, "and", name2, "are the youngest")
+    else:
+        print(name3, "is the youngest")
+
+elif age1 == age3:
+    if age1 < age2:
+        print(name1, "and", name3, "are the youngest")
+    else:
+        print(name2, "is the youngest")
+
+elif age2 == age3:
+    if age2 < age1:
+        print(name2, "and", name3, "are the youngest")
+    else:
+        print(name1, "is the youngest")
+
+else:
+    if age1 < age2 and age1 < age3:
+        print(name1, "is the youngest")
+    elif age2 < age1 and age2 < age3:
+        print(name2, "is the youngest")
+    else:
+        print(name3, "is the youngest")
