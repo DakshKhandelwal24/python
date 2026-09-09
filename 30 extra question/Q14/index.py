2eb761971eeed0b6f295370ed694df566f1a6331
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-09 09:39:21 +0530

    added

--- 30 extra question/Q14/index.py
+++ 30 extra question/Q14/index.py
@@ -1,3 +1,11 @@
-if __name__ == '__main__':
-    pass
+cost_price = int(input("enter the cost price :"))
+selling_price = int(input("enter the selling price:"))
 
+if cost_price>=selling_price:
+    print("its a loss")
+elif selling_price>=cost_price:
+    print("its a profit")
+elif selling_price==cost_price:
+    print("no profit and no loss")
+else:
+    print("invalid")
--- 30 extra question/Q18/index.py
+++ 30 extra question/Q18/index.py
@@ -1,3 +1,12 @@
-if __name__ == '__main__':
-    pass
+temperature = float(input("Enter temperature in Celsius: "))
 
+if temperature < 0:
+    print("Freezing")
+elif temperature <= 15:
+    print("Very Cold")
+elif temperature <= 25:
+    print("Cold")
+elif temperature <= 35:
+    print("Normal")
+else:
+    print("Hot")
