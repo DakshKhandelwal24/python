c28af160239c1ac476022f1ba1503df9be6814d5
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-09 15:40:26 +0530

    added soln

--- 30 extra question/Q23/index.py
+++ 30 extra question/Q23/index.py
@@ -1,3 +1,13 @@
-if __name__ == '__main__':
-    pass
+username=input("entre username")
+password=input("entre password")
+Username = input("enter the username:")
+Password = input("enter the password:")
 
+if Username=="admin" and Password=="python123":
+    print("login successful")
+elif Password!="python123":
+    print("Wrong password")
+elif Username!="admin":
+    print("User not found")
+else:
+    print("please try again")
--- 30 extra question/Q25/index.py
+++ 30 extra question/Q25/index.py
@@ -1,3 +1,12 @@
-if __name__ == '__main__':
-    pass
-
+marks=int(input("entre your marks"))
+if marks<=100 and marks>=75:
+    print("Distinction")
+elif marks<75 and marks>=60:
+    print("first class")
+elif marks<60 and marks>50:
+    print("second class")
+elif marks<=49 and marks>35:
+    print("pass")
+else :
+    print(("Fail"))    
+    
