7a4656d66c2eafb960f4aa58f96015ed38b9e2b8
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-15 15:54:17 +0530

    added

--- 10  for loop/class.py
+++ 10  for loop/class.py
@@ -1,3 +1,28 @@
 # # num=int(input("entre input in integer"))
 # for num in range(1,11):
-#         print(18*num)
+# #         print(18*num)
+# name=input("entre a string").strip().lower()
+# lenght=len(name)
+# sum=""
+# for number in range (lenght-1,-1,-1):
+#     sum=sum+name[number]
+# print(sum)
+
+
+# if name==sum:
+#         print("string is palindrome")
+# else :
+# #         print("string is not palindrome")    
+# for i in range(3):
+#     for j in range(2):
+# #         print(i, j)
+# for i in range(4):
+#     for j in range(4):
+#         print("*", end="")
+# #     print("")    
+# for k in range(4):
+#     print("*"*4)
+for i in range(1,6):
+    for k in range(i):
+        print("*",end="")
+    print("")    
