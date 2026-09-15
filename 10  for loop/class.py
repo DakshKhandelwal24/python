430b6fc316acd8bf1d4353cfb4679e9f5b90d12f
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-15 16:11:33 +0530

    added

--- 10  for loop/class.py
+++ 10  for loop/class.py
@@ -22,7 +22,5 @@
 # #     print("")    
 # for k in range(4):
 #     print("*"*4)
-for i in range(1,6):
-    for k in range(i):
-        print("*",end="")
-    print("")    
+for i in range(4,0,-1):
+    print("*"*i)
