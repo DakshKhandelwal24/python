c19a44618c7fbbf967980356bbbf753c1632eb3e
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-24 17:58:08 +0530

    added files

--- 13 patter problem of python/index.py
+++ 13 patter problem of python/index.py
@@ -4,9 +4,62 @@
 #         print("*", end=" ")
 #     print()
 
+
+
 # question 2
 # num=int(input("enter your input"))
 # for i in range(1,4):
 #     for k in range(1,4):
 #         print(k, end=" ")
 #     print()
+
+
+
+# question 3
+# num=int(input("enter your input"))
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(i,end=" ")
+#     print() 
+
+
+# question 4
+# for i in range (1,6):
+#     for k in range(i):
+#         print("*",end=" ")
+#     print()
+
+# Question 5
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()    
+
+# Question 6
+# num=int(input("enter till where uou want to print number"))
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#       print(j,end=" ")
+#     print()  
+
+# # Question 7
+# num=int(input("enter number till where you want to print:"))
+# for i in range(1,6):
+#     for k in range(i):
+#         print(i,end=" ")
+#     print()    
+
+
+# Question 8
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(f"{i} X {j} = {i*j}")
+#     print()    
+
+# Question 9
+# for i in range(1,4):
+#     for j in range(1,6):
+#         print(f"{i*j}" ,end=" ")
+#     print()    
+
+# question 10
