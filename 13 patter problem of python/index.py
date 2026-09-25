09594c1024dc664227939d59d6d59769dc971fc5
DakshKhandelwal24 <daksh.khandelwal.cg@gmail.com>
2026-09-25 10:30:00 +0530

    added files

--- 13 patter problem of python/index.py
+++ 13 patter problem of python/index.py
@@ -69,4 +69,110 @@
 #     print()   
 # 
 # quesion 11
-  
+# for i in range(1,6):
+#     for j in range(i):
+#         print(chr(65+j),end=" ")
+#     print()    
+
+# question 12
+# for i in range(1,6):
+#     for j in range(i):
+#         print(chr(64+i),end=" ")
+#     print()    
+
+# Question 13
+# num = int(input("enter a number :"))
+# for i in range(0,num+1,2):
+#     for j in range(1,i,2):
+#         print(j,end=" ")
+#     print()
+
+
+# # Question 14
+# num = int(input("enter a number :"))
+# for i in range(2,num,2):
+#     for j in range(2,i,2):
+#         print(j,end="")
+#     print()
+
+
+#     # question 15
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print("*",end=" ")
+#     print()            
+
+#     # QUEStion 16
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(j,end=" ")
+#     print()
+
+#     # question 17
+#     num = int(input("enter a number :"))
+# for i in range(1,num,3):
+#     for j in range(i,i+3):
+#         print(j,end=" ")
+#     print()
+#     # question 18
+#     num = int(input("enter a number :"))
+# for i in range(1,21,5):
+#     for j in range(i,i+5):
+#         print(j,end=" ")
+#     print()
+#     # Question 19
+#     num = int(input("enter a number :"))
+# for i in range(1,num+1):
+#     for j in range(1,num+1):
+#         print(f"({i},{j})",end=" ")
+#     print()
+#     # question 20
+#     num = int(input("enter a number :"))
+# for i in range(1,num+1):
+#     for j in range(1,num+1):
+#         print(i,j)
+
+#     # question 21
+
+
+
+
+
+
+#     # Question 22
+#     num = int(input("enter a number :"))
+# for i in range(1,num+1):
+#     for j in range(1,i+1):
+#         print(i,end="")
+#     print()
+
+#     # question 23
+#     num = int(input("enter a number :"))
+# for i in range(num,0,-1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+#     # Question 24
+#     num = int(input("enter a number :"))
+# for i in range(num,0,-1):
+#     for j in range(0,i):
+#         if j==1:
+#             print("5",end="")
+#         elif j==2:
+#             print("4",end="")
+#         elif j==3:
+#             print("3",end="")
+#         elif j==4:
+#             print("2",end="")
+#         elif j==5:
+#             print("1",end="")
+#     print()
+#     # Question 25
+#     num = int(input("enter a number :"))
+# for i in range(1,num+1):
+#     for j in range(1,num+1):
+#         print(i,end="")
+#     print()
+
+
